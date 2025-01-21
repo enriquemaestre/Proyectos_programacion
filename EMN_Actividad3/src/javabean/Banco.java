be56e8d2c890abf8adbad7dc62b07232dc352855
Enrique Maestre <enrique.maestre265@comunidadunir.net>
2025-01-21 19:33:46 +0100

    ADD: se incluye fichero Javadoc

--- EMN_Actividad3/src/javabean/Banco.java
+++ EMN_Actividad3/src/javabean/Banco.java
@@ -5,10 +5,28 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+/**
+ * Clase que proporciona el objeto Banco
+ * @author enriquemaestre
+ * @version 1.0
+ */
+
+/**
+ * Usamos el programa lombok para que genere los metodos inherentes:
+ *  constructores con todo,con nada, equals y hashcode y ToString
+ */
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode (of = "codigo")
+
+/**
+ * Para la clase Banco necesitamos un numero entero que indique su 
+ * codigo, un String para indicar el nombre y por ultimo un objeto 
+ * de la clase @see Pais.java para indicar el pais en el que opera.
+ */
+
 public class Banco {
 	private int codigo;
 	private String nombre;
